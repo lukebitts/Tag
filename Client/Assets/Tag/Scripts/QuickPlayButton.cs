3c7fd9a8c75b79003f7cf7c452dd309910d3e111
Lucas Bittencourt de Souza <lbittencs@gmail.com>
2016-08-29 03:12:25 -0300

    User can connect and login as a guest on the server

---
+++ Client/Assets/Tag/Scripts/QuickPlayButton.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Entities.Data;
+
+public class QuickPlayButton : MonoBehaviour {
+
+    SmartFox sfs;
+
+    void SetupListeners()
+    {
+        sfs.AddEventListener(SFSEvent.CONNECTION, OnConnection);
+        sfs.AddEventListener(SFSEvent.CONNECTION_LOST, OnConnectionLost);
+        sfs.AddEventListener(SFSEvent.LOGIN, OnLogin);
+        sfs.AddEventListener(SFSEvent.LOGIN_ERROR, OnLoginError);
+    }
+
+    void Reset()
+    {
+        sfs = Connection.Instance().Reset();
+        SetupListeners();
+    }
+
+    void Start()
+    {
+        Reset();
+    }
+
+    public void OnClick()
+    {
+        sfs.Connect("127.0.0.1", 9933);
+    }
+
+    void OnConnection(BaseEvent evt)
+    {
+        bool connectionSuccess = (bool)evt.Params["success"];
+
+        Debug.Log("OnConn:" + connectionSuccess);
+
+        if (connectionSuccess)
+        {
+            sfs.Send(new Sfs2X.Requests.LoginRequest("", "", "Lobby"));
+        }
+        else
+        {
+            Reset();
+        }
+    }
+
+    void OnConnectionLost(BaseEvent evt)
+    {
+        string reason = evt.Params["reason"] as string;
+        print("Connection lost: " + reason);
+
+        Reset();
+    }
+
+    void OnLogin(BaseEvent evt)
+    {
+        var user = (Sfs2X.Entities.User)evt.Params["user"];
+        Debug.Log("Login success. " + user.Name);
+    }
+
+    void OnLoginError(BaseEvent evt)
+    {
+        Debug.Log("Login error: " + evt.Params["errorMessage"] + " - Code: " + evt.Params["errorCode"]);
+        sfs.Disconnect();
+    }
+}
